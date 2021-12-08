2f4d46c37b0185053f22f94e85e11d7418a4db81
palkovics adam <pa.adam94@gmail.com>
2021-12-08 17:43:52 +0100

    day03 part2 -> oxy works

--- src/puzzles/day03/mod.rs
+++ src/puzzles/day03/mod.rs
@@ -1,11 +1,173 @@
 pub fn name() -> Option<String> {
-    None
+    Some(String::from("Binary Diagnostic"))
 }
 
 pub fn solve_p1() -> Option<String> {
-    None
+    let input = include_str!("input.txt");
+    Some(get_solution_p1(input))
 }
 
 pub fn solve_p2() -> Option<String> {
     None
+}
+
+pub fn get_solution_p1(input: &str) -> String {
+    let mut counts = Vec::new();
+    let mut line_count = 0;
+    let mut gamma_vec = Vec::new();
+    let mut epsilon_vec = Vec::new();
+    let gamma;
+    let epsilon;
+
+    let mut lines = input.lines();
+    for chr in lines.next().unwrap().chars() {
+	counts.push(chr.to_digit(10).unwrap());
+	gamma_vec.push(0);
+	epsilon_vec.push(0);
+    }
+    line_count += 1;
+    for line in lines {
+	for (i, chr) in line.chars().enumerate() {
+	    if chr == '1' {
+		counts[i] += 1;
+	    }
+	}
+	line_count += 1;
+    }
+
+    for i in 0..counts.len() {
+	if counts[i] >= line_count / 2 {
+	    gamma_vec[i] = 1;
+	} else {
+	    epsilon_vec[i] = 1;
+	}
+    }
+    gamma = bin_vec_to_dec(gamma_vec);
+    epsilon = bin_vec_to_dec(epsilon_vec);
+    format!("{}", gamma * epsilon)
+}
+
+pub fn get_part_p2(input: &str, oxy: bool) -> i32 {
+    let mut node = &mut Node::new();
+    let mut res_string = String::new();
+    let mut cmp: i32;
+    for line in input.lines() {
+	node.add_str_to_tree(line);
+    }
+    loop {
+	if node.get_side_node(0).get_count() == 0 &&
+	    node.get_side_node(1).get_count() == 0 {
+		break;
+	    }
+	cmp = node.get_side_node(0).get_count() -
+	    node.get_side_node(1).get_count();
+	if oxy {
+	    match cmp {
+		i32::MIN..=-1 => {
+		    node = node.get_side_node(1);
+		    res_string += "1";
+		}
+		1..=i32::MAX => {
+		    node = node.get_side_node(0);
+		    res_string += "0";
+		}
+		0 => {
+		    node = node.get_side_node(1);
+		    res_string += "1";
+		}
+	    }
+	} else {
+	    match cmp {
+		i32::MIN..=-1 => {
+		    node = node.get_side_node(0);
+		    res_string += "0";
+		}
+		1..=i32::MAX=> {
+		    node = node.get_side_node(1);
+		    res_string += "1";
+		}
+		0  => {
+		    node = node.get_side_node(1);
+		    res_string += "0";
+		}
+	    }
+	}
+    }
+    println!("{}", res_string);
+    i32::from_str_radix(&res_string, 2).
+	expect(&(format!("couldn't convert {} to decimal", &res_string)))
+}
+
+pub struct Node {
+    count: i32,
+    left_0: Box<Option<Node>>,
+    right_1: Box<Option<Node>>,
+}
+
+impl Node {
+    pub fn new()-> Node {
+	Node { count: 0,
+	       left_0: Box::new(None),
+	       right_1: Box::new(None)
+	}
+    }
+
+    pub fn add_node(&mut self, side: i8) {
+	let new = Node::new();
+	match side {
+	    0 => self.left_0 = Box::new(Some(new)),
+	    1 => self.right_1 = Box::new(Some(new)),
+	    _ => panic!("bad side parameter `{}`", side)
+	}
+    }
+
+    pub fn get_side_node(&mut self, side: i8) -> &mut Node {
+	match side {
+	    0 => match self.left_0.as_ref().as_ref() {
+		Some(_) => self.left_0.as_mut().as_mut().unwrap(),
+		None => {
+		    self.add_node(0);
+		    self.left_0.as_mut().as_mut().unwrap()
+		}
+	    },
+	    1 => match self.right_1.as_ref().as_ref() {
+		Some(_) => self.right_1.as_mut().as_mut().unwrap(),
+		None => {
+		    self.add_node(1);
+		    self.right_1.as_mut().as_mut().unwrap()
+		}
+	    }
+	    _ => panic!("bad side parameter `{}`", side),
+	}
+    }
+
+    pub fn add_str_to_tree(&mut self, s: &str) {
+	let mut side: i8;
+	let mut curr_node = self;
+	curr_node.inc_count();
+	for chr in s.chars() {
+	    side = chr.to_digit(10).unwrap().try_into().unwrap();
+	    curr_node = curr_node.get_side_node(side);
+	    curr_node.inc_count();
+	}
+    }
+
+    pub fn inc_count(&mut self) {
+	self.count += 1
+    }
+
+    pub fn get_count(&self) -> i32 {
+	self.count
+    }
+}
+
+pub fn bin_vec_to_dec(mut bin: Vec<i32>) -> i32 {
+    let mut ret_val = 0;
+    bin.reverse();
+    for (i, value) in bin.iter().enumerate() {
+	if value.to_owned() == 1 {
+	    ret_val += 2_i32.pow(i.try_into().unwrap());
+	}
+    }
+    ret_val
 }
--- tests/solution_test.rs
+++ tests/solution_test.rs
@@ -1,31 +1,89 @@
 mod day1 {
     use aoc2021::puzzles::day01::*;
 
+    fn get_input() -> &'static str{
+	include_str!("day1.txt")
+    }
+
     #[test]
     fn solve_part1() {
-	let input = include_str!("day1.txt");
-	assert_eq!("7", get_solution_p1(input));
+	assert_eq!("7", get_solution_p1(get_input()));
     }
 
     #[test]
     fn solve_part2() {
-	let input = include_str!("day1.txt");
-	assert_eq!("5", get_solution_p2(input));
+	assert_eq!("5", get_solution_p2(get_input()));
     }
 }
 
 mod day2 {
     use aoc2021::puzzles::day02::*;
 
+    fn get_input() -> &'static str{
+	include_str!("day2.txt")
+    }
+
     #[test]
     fn solve_part1() {
-	let input = include_str!("day2.txt");
-	assert_eq!("150", get_solution_p1(input));
+	assert_eq!("150", get_solution_p1(get_input()));
     }
 
     #[test]
     fn solve_part2() {
-	let input = include_str!("day2.txt");
-	assert_eq!("900", get_solution_p2(input));
+	assert_eq!("900", get_solution_p2(get_input()));
+    }
+}
+
+mod day3 {
+    use aoc2021::puzzles::day03::*;
+
+    fn get_input() -> &'static str{
+	include_str!("day3.txt")
+    }
+
+    #[test]
+    fn test_bin_to_vec() {
+	assert_eq!(58, bin_vec_to_dec(vec!(0,0,0,1,1,1,0,1,0)));
+    }
+
+    #[test]
+    fn solve_part1() {
+	assert_eq!("198", get_solution_p1(get_input()));
+    }
+
+    #[test]
+    fn test_node_add() {
+	let mut root = Node::new();
+	root.add_node(0);
+	root.get_side_node(0).inc_count();
+	assert_eq!(root.get_side_node(0).get_count(), 1);
+    }
+
+    #[test]
+    fn test_add_str_to_tree() {
+	let mut res_vec = vec!();
+	let mut node = &mut Node::new();
+	node.add_str_to_tree("1001");
+	node.add_str_to_tree("1011");
+	res_vec.push(node.get_count());
+	node = node.get_side_node(1);
+	res_vec.push(node.get_count());
+	node = node.get_side_node(0);
+	res_vec.push(node.get_count());
+	node = node.get_side_node(0);
+	res_vec.push(node.get_count());
+	node = node.get_side_node(1);
+	res_vec.push(node.get_count());
+	assert_eq!(vec!(2,2,2,1,1), res_vec);
+    }
+
+    #[test]
+    fn test_part_oxy() {
+	assert_eq!(23, get_part_p2(get_input(), true));
+    }
+
+    #[test]
+    fn test_part_co() {
+	assert_eq!(10, get_part_p2(get_input(), false));
     }
 }
