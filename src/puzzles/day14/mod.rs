3a78ce315470a900bf7ef53dbbdccee188cfaaf3
Palkovics Adam <pa.adam94@gmail.com>
2022-01-03 16:12:09 +0100

    Revert "day14 readinput implemented"
    This reverts commit 869ae0ee9ede6a4b77cf4d247b68f33c204bbef5.

--- src/puzzles/day14/mod.rs
+++ src/puzzles/day14/mod.rs
@@ -1,7 +1,5 @@
-use std::collections::HashMap;
-
 pub fn name() -> Option<String> {
-    Some("Extended Polymerization".to_string())
+    None
 }
 
 pub fn solve_p1() -> Option<String> {
@@ -12,48 +10,12 @@ pub fn solve_p2() -> Option<String> {
     None
 }
 
-struct Input {
-    base: String,
-    rules: HashMap<String, String>,
-}
-
-impl Input {
-    fn new(input: &str) -> Input {
-        let mut base: String = String::new();
-        let mut rules: HashMap<String, String> = HashMap::new();
-        let mut lines = input.lines();
-        base = lines.next().unwrap().to_string();
-        while let Some(t) = lines.next() {
-            let mut splitline = t.split("->");
-            let mut from = String::new();
-            match splitline.next() {
-                None => continue,
-                Some(t) if t.len() < 2 => {
-		    continue;
-                }
-		Some(t) => {
-                    from = t.to_string();
-		}
-            }
-            let to = splitline.next().unwrap().to_string();
-            rules.insert(from, to);
-        }
-        Input { base, rules }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     #[allow(unused_imports)]
     use super::*;
 
-    fn get_input() -> &'static str {
-        include_str!("input_test.txt")
-    }
-
-    #[test]
-    fn test_parse() {
-        let input = Input::new(get_input());
-        panic!();
-    }
+    // fn get_input() -> &'static str {
+    // 	include_str!("input_test.txt")
+    // }
 }
