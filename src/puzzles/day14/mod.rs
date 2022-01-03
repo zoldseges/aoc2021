c248a01a6ad39fc0ec861e95c947e966b3aa7955
Palkovics Adam <pa.adam94@gmail.com>
2022-01-03 16:15:30 +0100

    reverting back to 896a, (revert multithreading since it didn't bring any speedup)

--- src/puzzles/day14/mod.rs
+++ src/puzzles/day14/mod.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
+
 pub fn name() -> Option<String> {
-    None
+    Some("Extended Polymerization".to_string())
 }
 
 pub fn solve_p1() -> Option<String> {
@@ -10,12 +12,48 @@ pub fn solve_p2() -> Option<String> {
     None
 }
 
+struct Input {
+    base: String,
+    rules: HashMap<String, String>,
+}
+
+impl Input {
+    fn new(input: &str) -> Input {
+        let mut base: String = String::new();
+        let mut rules: HashMap<String, String> = HashMap::new();
+        let mut lines = input.lines();
+        base = lines.next().unwrap().to_string();
+        while let Some(t) = lines.next() {
+            let mut splitline = t.split("->");
+            let mut from = String::new();
+            match splitline.next() {
+                None => continue,
+                Some(t) if t.len() < 2 => {
+		    continue;
+                }
+		Some(t) => {
+                    from = t.to_string();
+		}
+            }
+            let to = splitline.next().unwrap().to_string();
+            rules.insert(from, to);
+        }
+        Input { base, rules }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[allow(unused_imports)]
     use super::*;
 
-    // fn get_input() -> &'static str {
-    // 	include_str!("input_test.txt")
-    // }
+    fn get_input() -> &'static str {
+        include_str!("input_test.txt")
+    }
+
+    #[test]
+    fn test_parse() {
+        let input = Input::new(get_input());
+        panic!();
+    }
 }
